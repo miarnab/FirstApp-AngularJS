e3dfa094ebdfd506618fee11b41a8e0a4b2bacfd
Arnab Mitra <mitraarnab.2013@gmail.com>
2022-07-29 11:58:13 +0530

    Feature Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'First Project';
+  color = "#";
+  letters = '0123456789ABCDEF';
+
+  getRandomColour(){
+    this.color = '#';
+    for (var i = 0; i < 6; i++) {
+        this.color += this.letters[Math.floor(Math.random() * 16)];
+    }
+  }
 }
